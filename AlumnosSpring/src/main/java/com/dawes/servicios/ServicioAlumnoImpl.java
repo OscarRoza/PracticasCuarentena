8d052e79b2dda21cbd754f96b9a2c6d7c08de124
Sinner <oscarrf66@educastur.es>
2020-03-30 17:43:46 +0200

    añadidos los Servicios

---
+++ AlumnosSpring/src/main/java/com/dawes/servicios/ServicioAlumnoImpl.java
@@ -0,0 +1,84 @@
+package com.dawes.servicios;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dawes.modelo.AlumnoVO;
+import com.dawes.repositorios.AlumnoRepository;
+
+@Service
+public class ServicioAlumnoImpl implements ServicioAlumno {
+
+	@Autowired
+	AlumnoRepository AR;
+
+	@Override
+	public AlumnoVO BuscarPorNombre(String nombre) {
+		return AR.BuscarPorNombre(nombre);
+	}
+
+	@Override
+	public List<AlumnoVO> findAllByCursosFechaBetween(LocalDate inicio, LocalDate fin) {
+		return AR.findAllByCursosFechaBetween(inicio, fin);
+	}
+
+	@Override
+	public <S extends AlumnoVO> S save(S entity) {
+		return AR.save(entity);
+	}
+
+	@Override
+	public <S extends AlumnoVO> Iterable<S> saveAll(Iterable<S> entities) {
+		return AR.saveAll(entities);
+	}
+
+	@Override
+	public Optional<AlumnoVO> findById(Integer id) {
+		return AR.findById(id);
+	}
+
+	@Override
+	public boolean existsById(Integer id) {
+		return AR.existsById(id);
+	}
+
+	@Override
+	public Iterable<AlumnoVO> findAll() {
+		return AR.findAll();
+	}
+
+	@Override
+	public Iterable<AlumnoVO> findAllById(Iterable<Integer> ids) {
+		return AR.findAllById(ids);
+	}
+
+	@Override
+	public long count() {
+		return AR.count();
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		AR.deleteById(id);
+	}
+
+	@Override
+	public void delete(AlumnoVO entity) {
+		AR.delete(entity);
+	}
+
+	@Override
+	public void deleteAll(Iterable<? extends AlumnoVO> entities) {
+		AR.deleteAll(entities);
+	}
+
+	@Override
+	public void deleteAll() {
+		AR.deleteAll();
+	}
+
+}
