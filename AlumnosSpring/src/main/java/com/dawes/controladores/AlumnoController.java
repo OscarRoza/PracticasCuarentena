8c1a3f5c7d867c7fecb85226b91045c16eade138
Sinner <oscarrf66@educastur.es>
2020-04-01 19:01:16 +0200

    Inserccion de usuarios activa

--- AlumnosSpring/src/main/java/com/dawes/controladores/AlumnoController.java
+++ AlumnosSpring/src/main/java/com/dawes/controladores/AlumnoController.java
@@ -3,9 +3,12 @@ package com.dawes.controladores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.dawes.modelo.AlumnoVO;
 import com.dawes.servicios.ServicioAlumnoImpl;
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 
 @Controller
 @RequestMapping("/controladorAlumno")
@@ -16,7 +19,21 @@ public class AlumnoController {
 
 	@RequestMapping("/listaAlumno")
 	public String listaAlumno(Model modelo) {
-		modelo.addAttribute("alumnos", sa.findAll());
+		modelo.addAttribute("listaAlumno", sa.findAll());
 		return "/alumno/mostrarAlumnos";
+	}
+
+	@RequestMapping("/insertarAlumno")
+	public String insertarAlumno(Model modelo) {
+		modelo.addAttribute("alumno", new AlumnoVO());
+		return "/alumno/insertarAlumno";
+	}
+
+	@RequestMapping("/inserccionAlumno")
+	public String inserccionAlumno(@ModelAttribute AlumnoVO alumno, Model modelo) {
+		sa.save(alumno);
+		modelo.addAttribute("listaAlumno", sa.findAll());
+		return "/alumno/mostrarAlumnos";
+
 	}
 }
