35e2cc48bec58a475b811eee21428ad97595c9dc
Sinner <oscarrf66@educastur.es>
2020-04-02 19:45:53 +0200

    acabado

--- AlumnosSpring/src/main/resources/META-INF/sql/data.sql
+++ AlumnosSpring/src/main/resources/META-INF/sql/data.sql
@@ -10,5 +10,5 @@ insert into curso (nombrecurso) values ("JAVA");
 
 insert into alumnocurso (fecha, idalumno, idcurso) values ('2019-09-10',1,1);
 insert into alumnocurso (fecha, idalumno, idcurso) values ('2019-09-5',1,2);
-insert into alumnocurso (fecha, idalumno, idcurso) values ('2019-09-1',4,2);
-insert into alumnocurso (fecha, idalumno, idcurso) values ('2019-09-22',3,2);
+insert into alumnocurso (fecha, idalumno, idcurso) values ('2019-09-1',3,2);
+insert into alumnocurso (fecha, idalumno, idcurso) values ('2019-09-22',4,2);
