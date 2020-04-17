539d20ade82ce5650b8fae0e2e50aafeff4775ab
Sinner <oscarrf66@educastur.es>
2020-04-17 19:10:00 +0200

    vistas y controladores

--- Oscar_REC_2EV/src/main/java/com/dawes/controller/ImpuestosCrontroller.java
+++ Oscar_REC_2EV/src/main/java/com/dawes/controller/ImpuestosCrontroller.java
@@ -3,12 +3,14 @@ package com.dawes.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.dawes.modelo.ImpuestosVO;
 import com.dawes.servicios.ServicioImpuestosImpl;
 
 @Controller
-@RequestMapping("controladorImpuestos")
+@RequestMapping("/controladorImpuestos")
 public class ImpuestosCrontroller {
 
 	@Autowired
@@ -18,5 +20,19 @@ public class ImpuestosCrontroller {
 	public String listaImpuestos(Model modelo) {
 		modelo.addAttribute("listaImpuestos", si.findAll());
 		return "/impuestos/listaImpuestos";
+	}
+
+	@RequestMapping("/insertarImpuesto")
+	public String insertarImpuesto(Model modelo) {
+		modelo.addAttribute("impuestos", new ImpuestosVO());
+		return "/impuestos/formInsertarImpuesto";
+	}
+
+	@RequestMapping("/inserccionImpuesto")
+	public String inserccionAlumno(@ModelAttribute ImpuestosVO impuestos, Model modelo) {
+		si.save(impuestos);
+		modelo.addAttribute("listaAlumno", si.findAll());
+		return "/impuestos/listaImpuestos";
+
 	}
 }
---
+++ Oscar_REC_2EV/src/main/java/com/dawes/controller/MenuController.java
@@ -0,0 +1,15 @@
+package com.dawes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MenuController {
+
+	@RequestMapping("/index")
+	public String menuPrincipal() {
+		return "index";
+	}
+}
+
